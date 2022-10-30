8ffc5a555d143e86805e8e61def3e516a7a2f590
Tiarasekarsini <tiara.sekarsini.ft20@mail.umy.ac.id>
2022-10-30 21:49:52 +0700

    Mengubah index.html dan view.html untuk form dan menampilkan gambar serta mengubah method di controller

--- src/main/java/ws/a/takehome1/iniController.java
+++ src/main/java/ws/a/takehome1/iniController.java
@@ -5,12 +5,17 @@
  */
 package ws.a.takehome1;
 
-import java.sql.Blob;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -19,19 +24,25 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class iniController {
+    public static String UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/src/main/resources/static";
     @RequestMapping("/nexthealing")
     
     public String healduls(
             @RequestParam(value = "nTempatwisata") String form1,
             @RequestParam(value = "lokasiTW") String form2,
-            @RequestParam(value = "gambarTW") String form3,
+            @RequestParam(value = "gambarTW") MultipartFile form3,
             Model kurir
-    ){
+    )throws IOException {
+        
+        StringBuilder fileNames = new StringBuilder();
+        Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, form3.getOriginalFilename());
+        fileNames.append(form3.getOriginalFilename());
+        Files.write(fileNameAndPath, form3.getBytes());
+        
         kurir.addAttribute("pTW", form1);
         kurir.addAttribute("pLokasi",form2);
-        kurir.addAttribute("pgambarTW", form3);
+        kurir.addAttribute("pgambarTW", form3.getOriginalFilename());
         
         return "view";
-    
     }    
 }
